cc5d264d0929a09e447c8ba9eadcc390f76ba67b
0procod <promiseawelle@gmail.com>
2022-10-18 00:40:29 -0700

    all first pushes on C Intro

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+/**
+ * main - Print a string
+ * Description: use printf
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
